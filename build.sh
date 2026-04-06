f66d19793cfee13d01bf39d0a5decc7895eaa2a7
Tim Newsome <tim@casualhacker.net>
2026-04-05 21:49:54 -0700

    Compile with draco by default

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 wails build -tags draco
-go build -o ditherforge-cli ./cmd/ditherforge
+go build -tags draco -o ditherforge-cli ./cmd/ditherforge
 echo "Built build/bin/ditherforge (GUI) and ditherforge-cli (CLI)"
